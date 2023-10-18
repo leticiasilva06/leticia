798824d6c614f5ac02f8914cc0210b41c07886d4
CARLOSbeserra <carloshcarvalho200999@gmail.com>
2023-10-18 08:51:57 -0300

    calculo

---
+++ script.js
@@ -0,0 +1,42 @@
+function calcularDoisNumeros(num1,num2,operacao){
+
+    let resposta
+
+    if (operacao === "*") {
+
+        resposta = multiplicar(num1,num2)
+
+    }else if(operacao === "/"){
+
+        resposta = dividir(num1,num2)
+
+    }else if(operacao === "-"){
+
+        resposta = subtrair(num1,num2)
+
+    }else if(operacao === "+"){
+
+        resposta = somar(num1,num2)
+        
+    }
+
+     
+    document.getElementById("resultado").innerHTML = resposta
+}
+
+function multiplicar(num1,num2){
+    return num1 * num2
+}
+
+function dividir(num1,num2){
+    return num1/num2
+}
+
+function subtrair(num1,num2){
+    return num1-num2
+}
+
+function somar(num1,num2){
+    return num1+num2
+}
+ 
