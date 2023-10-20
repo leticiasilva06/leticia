ecf9884929413d67f3832ac53baa9c3f84e72450
Leticia da Silva <leticia0612soliveira@gmail.com>
2023-10-20 11:28:46 -0300

    commit

--- calcular.js
+++ calcular.js
@@ -1,3 +1,5 @@
+// calculadora 
+
 function calcularDoisNumeros(operacao){
     let num1 = Number(document.getElementById('num1').value)
     let num2 = Number(document.getElementById('num2').value)
@@ -23,12 +25,31 @@ function calcularDoisNumeros(operacao){
 // tabuada
 
 const tabuada = document.getElementById("tabuada");
-function mostrarTabuada(){
+function mostrarTabuada() {
+
     let numero = Number(document.getElementById("numero").value);
 
     let resposta = "";
-    for (let i = 1; i<= 10; i++){
+    for (let i = 1; i <= 10; i++) {
         resposta += numero + "x" + i + "=" + numero * i + "<br>"; 
     }
-    tabuada.innerHTML = resposta
+    tabuada.innerHTML = resposta;
+}
+
+// lista de alunos
+
+const alunos = ["Sara", "Abner", "Guilherme", "Chrystian", "Victória"];
+
+function listaAlunos() {
+    
+
+    let i = 0;
+    let text = "";
+    while (i < alunos.length) {
+      text += "<li>" + alunos[i] + "</li>";
+      i++;
+    }
+                
+
+document.getElementById("alunos").innerHTML = text
 }
